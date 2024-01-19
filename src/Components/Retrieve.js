c8ae7ba197d40d31408d37ac7c58cedb2478bf70
Kaveri Chougule <chougulekaveri123@gmail.com>
2024-01-20 00:51:01 +0530

    Initial Commit

--- src/Components/Retrieve.js
+++ src/Components/Retrieve.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useReducer, useRef, useState } from "react";
+import Table from "./Table";
 
 function Retrieve(props) {
+    let [getData,setData]=useState([]);
+    let getInput=useRef(null);
+    function SearchUser(){
+        
+        setData(()=>{
+            let data=JSON.parse(localStorage.getItem('user'));
+            data=data.filter((ele)=>{
+                return ele.aadhar==getInput.current.value
+            })
+            return data
+        })
+    }
+    
   return (
-    <div className="Retrieve">
-      
+    <div className="App">  
+      <div className="MainContainer">
+        <div className="container">
+          <div>
+            <p>Retrieve Information</p>
+          </div>
+            <input type="number" placeholder="Aadhar Number" ref={getInput} />
+            <button onClick={SearchUser}>Find</button>
+
+            <Table LocalStorageData={getData} setData={setData} />
+        </div>
+        
+      </div>
     </div>
   );
 }
