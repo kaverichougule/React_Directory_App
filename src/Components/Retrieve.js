1e55e923054b4e5f7e9eea4ae9f76014ca9ffa89
Kaveri Chougule <chougulekaveri123@gmail.com>
2024-01-19 23:05:03 +0530

    Initial Commit

---
+++ src/Components/Retrieve.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Retrieve(props) {
+  return (
+    <div className="App">
+      <div className="Heading">
+        <h3>Kaveri Chougule Directory App</h3>
+      </div>
+      <div className="MainContainer">
+        <div className="buttons">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Retrieve;
